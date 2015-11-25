41f903a9e8620c71040d0c61f2cc9ad7aa6264db
Zac Sweers <zsweers@uber.com>
2015-11-25 00:17:00 -0800

    Use RxJava 1.0.16's snazzy fromCallable
    Kind of almost makes this library overkill

--- rxpalette/src/main/java/io/sweers/rxpalette/RxPalette.java
+++ rxpalette/src/main/java/io/sweers/rxpalette/RxPalette.java
@@ -5,6 +5,8 @@ import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.support.v7.graphics.Palette;
 
+import java.util.concurrent.Callable;
+
 import rx.Observable;
 
 /**
@@ -16,15 +18,25 @@ public final class RxPalette {
      * Generate the {@link Palette} synchronously.
      */
     @CheckResult @NonNull
-    public static Observable<Palette> generate(@NonNull Bitmap bitmap) {
-        return Observable.create(new GeneratePaletteOnSubscribe(bitmap));
+    public static Observable<Palette> generate(@NonNull final Bitmap bitmap) {
+        return Observable.fromCallable(new Callable<Palette>() {
+            @Override
+            public Palette call() throws Exception {
+                return Palette.from(bitmap).generate();
+            }
+        });
     }
 
     /**
      * Generate the {@link Palette} synchronously.
      */
     @CheckResult @NonNull
-    public static Observable<Palette> generate(@NonNull Palette.Builder builder) {
-        return Observable.create(new GeneratePaletteBuilderOnSubscribe(builder));
+    public static Observable<Palette> generate(@NonNull final Palette.Builder builder) {
+        return Observable.fromCallable(new Callable<Palette>() {
+            @Override
+            public Palette call() throws Exception {
+                return builder.generate();
+            }
+        });
     }
 }
