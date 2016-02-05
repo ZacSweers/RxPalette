9d346d4d46cd3b92b5551f2b4886b16cb67a8185
Zac Sweers <pandanomic@gmail.com>
2016-02-04 23:03:47 -0800

    Update kotlin, android gradle plugin, and retrofit

--- sample/src/main/java/io/sweers/rxpalette/sample/api/ImgurApi.java
+++ sample/src/main/java/io/sweers/rxpalette/sample/api/ImgurApi.java
@@ -8,9 +8,9 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import retrofit2.MoshiConverterFactory;
 import retrofit2.Retrofit;
-import retrofit2.RxJavaCallAdapterFactory;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.moshi.MoshiConverterFactory;
 import rx.Observable;
 
 /**
